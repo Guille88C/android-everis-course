6cc2b16ec812bf1327b70fb0670e7a012e2b59fd
Guillermo <thecrow88@gmail.com>
2019-11-20 12:59:29 +0100

    Global view created (missing files added).

---
+++ app/src/main/java/com/everis/myapplication/global/GlobalActivity.kt
@@ -0,0 +1,14 @@
+package com.everis.myapplication.global
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.everis.myapplication.R
+
+class GlobalActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_global)
+    }
+}
